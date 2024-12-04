d8b02e597f2f25655d6b8640f45c2d7296405f57
serge-yetimian <sergeyetimian2@gmail.com>
2024-12-04 15:14:15 +0000

    fifth commit

--- Untitled-2.py
+++ Untitled-2.py
@@ -13,3 +13,16 @@ class RecipeApp:
         self.window.geometry("600x600")  # Set window size
         self.window.configure(bg="#9ddfd3")
         self.window.title(WINDOW_TITLE)
+
+        # Search UI
+        self.search_label = tk.Label(self.window, text="Search Recipe", bg="#ea86b6")
+        self.search_label.grid(column=0, row=0, padx=5)
+
+        self.search_entry = tk.Entry(master=self.window, width=40)
+        self.search_entry.grid(column=1, row=0, padx=5, pady=10) 
+
+        self.search_button = tk.Button(self.window, text="Search", highlightbackground="#ea86b6",
+                                       command=self.run_search_query)
+        self.search_button.grid(column=2, row=0, padx=5)
+
+        
