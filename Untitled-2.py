220e68e3d22c1771a2dbf9c0623fd7cefc39df58
serge-yetimian <sergeyetimian2@gmail.com>
2024-12-04 15:13:03 +0000

    third commit

--- Untitled-2.py
+++ Untitled-2.py
@@ -2,3 +2,8 @@ import csv
 import tkinter as tk
 from PIL import Image, ImageTk  # Import Pillow for image handling
 import os
+
+# Constants
+WINDOW_TITLE = "Recipe App"
+CSV_FILE_PATH = "recipes.csv"
+
