b48408e05945dcfc5b1a3f8475681e45a5e9f045
serge-yetimian <sergeyetimian2@gmail.com>
2024-12-04 15:13:39 +0000

    fourth commit

--- Untitled-2.py
+++ Untitled-2.py
@@ -7,3 +7,9 @@ import os
 WINDOW_TITLE = "Recipe App"
 CSV_FILE_PATH = "recipes.csv"
 
+class RecipeApp:
+    def __init__(self):
+        self.window = tk.Tk()
+        self.window.geometry("600x600")  # Set window size
+        self.window.configure(bg="#9ddfd3")
+        self.window.title(WINDOW_TITLE)
